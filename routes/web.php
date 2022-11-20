5c67aa16bbfa0ac04d0c1a01668ed659db90f36f
Masanarea <93495976+Masanarea@users.noreply.github.com>
2022-11-20 17:46:59 +0900

    とりあえずバックアップ

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Home\HomeSliderController;
+use App\Http\Controllers\Home\AboutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -46,6 +47,11 @@ require __DIR__.'/auth.php';
  // Home Slide All Route 
 Route::controller(HomeSliderController::class)->group(function () {
     Route::get('/home/slide', 'HomeSlider')->name('home.slide');
+    Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
+});
 
-
+ // About Page All Route 
+Route::controller(AboutController::class)->group(function () {
+    Route::get('/about/page', 'AboutPage')->name('about.page');
+    Route::post('/update/about', 'UpdateAbout')->name('update.about');
 });
